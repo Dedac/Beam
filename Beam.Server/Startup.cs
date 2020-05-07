0adfca7bee5e334b950d8090c06313c1d6cdb595
Rich Goforth <rcgoforth@gmail.com>
2020-05-07 17:07:27 -0400

    Fix workspace and connection string for container

--- Beam.Server/Startup.cs
+++ Beam.Server/Startup.cs
@@ -17,17 +17,6 @@ namespace Beam.Server
 
         public IConfiguration Configuration { get; }
 
-
-        public Startup(IWebHostEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)

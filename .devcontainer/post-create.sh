65967bb6282d2eac501fde5ba33b1271f39ef8b8
Rich Goforth <rcgoforth@gmail.com>
2020-05-13 17:08:07 +0000

    Create database on post create

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+/opt/mssql-tools/bin/sqlcmd -l 60 -S sql -U SA -P "@#^!fcIen&*asd" -Q "CREATE DATABASE BEAM"
 dotnet restore 
 dotnet tool restore
 dotnet ef database update --project ./Beam.Server

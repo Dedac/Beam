14d6e1b951b1b58c35ec743e128a264115a18409
Rich Goforth <rcgoforth@gmail.com>
2020-05-24 22:04:41 -0400

    Use sdk 2.1.3

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 /opt/mssql-tools/bin/sqlcmd -l 60 -S sql -U SA -P "@#^!fcIen&*asd" -Q "CREATE DATABASE BEAM"
+cd /workspace
 dotnet restore 
 dotnet tool restore
 dotnet ef database update --project ./Beam.Server

b8fead9a2b23366a77f20c8ed4a04c1e748c2a5b
Unknown <rgoforth@ceiamerica.com>
2020-06-06 16:57:25 -0400

    Remove unnecessary post create commands

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-/opt/mssql-tools/bin/sqlcmd -l 60 -S sql -U SA -P "@#^!fcIen&*asd" -Q "CREATE DATABASE BEAM"
 cd /workspace
 dotnet restore
 cd Beam.Server
 dotnet tool restore
-dotnet ef database update --project ./Beam.Server
+dotnet ef database update 

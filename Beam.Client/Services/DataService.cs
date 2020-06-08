c45791e6131eeb128f0a98a96adec5ba2d3befc3
Rich Goforth <rcgoforth@gmail.com>
2020-06-07 22:23:34 -0400

    Complete tasks for 02_01

--- Beam.Client/Services/DataService.cs
+++ Beam.Client/Services/DataService.cs
@@ -55,6 +55,11 @@ namespace Beam.Client.Services
             UpdatedRays?.Invoke();
         }
 
+        public async Task<List<Ray>> GetMyRays()
+        {
+            return await http.GetFromJsonAsync<List<Ray>>($"api/Ray/user/{CurrentUser.Name}");
+        }
+
         public async Task AddFrequency(string Name)
         {
             Frequencies = await AddFrequency(new Frequency() { Name = Name });  
@@ -133,5 +138,6 @@ namespace Beam.Client.Services
         {
             return http.GetFromJsonAsync<List<Ray>>($"api/Prism/Remove/{userId}/{rayId}");
         }
+
     }
 }

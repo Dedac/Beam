c7a448e3440040c7378ceae1d990896732625276
Rich Goforth <rcgoforth@gmail.com>
2020-06-09 20:05:37 -0400

    fix launch settings for server

--- Beam.Client/Program.cs
+++ Beam.Client/Program.cs
@@ -14,11 +14,7 @@ namespace Beam.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-            builder.Services.AddSingleton(
-                new HttpClient
-                {
-                    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-                });
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSingleton<DataService>();
             await builder.Build().RunAsync();
         }

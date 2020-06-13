8611501b4878b70c668219b945153bb4f39dcb01
Rich Goforth <rcgoforth@gmail.com>
2020-06-13 10:15:45 -0400

    Add tests that support authorized view

--- Beam.Tests/IndexComponentTests.cs
+++ Beam.Tests/IndexComponentTests.cs
@@ -5,27 +5,25 @@ using Microsoft.AspNetCore.Authorization;
 using Beam.Client.Pages;
 using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Components.Authorization;
+using Beam.Tests.Auth;
 
 namespace Beam.Tests
 {
     public class IndexComponentTests : TestContext
     {
-        // [Fact]
-        // public void IndexPageRenders()
-        // {
-        //     Services.AddSingleton<IAuthorizationPolicyProvider, MockAuthorizationPolicyProvider>();
-        //     Services.AddSingleton<IAuthorizationService, MockAuthorizationService>();
-        //     Services.AddSingleton<AuthenticationStateProvider>(new TestAuthenticationStateProvider(null));
+        [Fact]
+        public void IndexPageRenders()
+        {
+            Services.AddAuthenticationServices(TestAuthenticationStateProvider.CreateAuthenticationState("TestUser"));
+            var wrapper = RenderComponent<CascadingAuthenticationState>(parameters => parameters.AddChildContent<Index>());
 
-        //     var wrapper = RenderComponent<CascadingAuthenticationState>(parameters => parameters.AddChildContent<Index>());
-
-        //     // Arrange
-        //     var cut = wrapper.FindComponent<Index>();
+            // Arrange
+            var cut = wrapper.FindComponent<Index>();
             
-        //     // Assert that content of the paragraph shows counter at zero
-        //     Assert.Contains("Select a Frequency, or create a new one", cut.Markup);
-        //     cut.MarkupMatches(@"<h2 diff:ignore></h2>
-        //                         <p diff:ignore></p>");
-        // }
+            // Assert that content of the paragraph shows counter at zero
+            Assert.Contains("Select a Frequency, or create a new one", cut.Markup);
+            cut.MarkupMatches(@"<h2 diff:ignore></h2>
+                                <p diff:ignore></p>");
+        }
     }
 }

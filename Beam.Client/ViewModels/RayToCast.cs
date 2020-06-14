5f60e8cc8a235a6d03a0759bb19c324cb9d91263
Rich Goforth <rcgoforth@gmail.com>
2020-06-14 15:26:23 -0400

    03_06 Challenge

---
+++ Beam.Client/ViewModels/RayToCast.cs
@@ -0,0 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+namespace Beam.Client.ViewModels {
+    public class RayToCast
+    {
+        [Required(ErrorMessage="You can't send an empty Ray!")]
+        public string Text { get; set; } = "";
+    }
+}

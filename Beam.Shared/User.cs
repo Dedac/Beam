91483b00069de5e7ab6c06eca38e4e1654a5ec63
Richard Goforth <rcgoforth@gmail.com>
2022-05-18 10:13:20 -0400

    Update User.cs

--- Beam.Shared/User.cs
+++ Beam.Shared/User.cs
@@ -6,6 +6,7 @@ namespace Beam.Shared
 {
     public class User
     {
+        public string token="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";
         public int Id { get; set; }
         public string Name { get; set; }
     }
